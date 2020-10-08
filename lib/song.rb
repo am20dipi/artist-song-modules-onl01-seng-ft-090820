79691a909d4a1ab227e7042d60e7beab266f1ce8
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-08 01:58:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,11 @@
 require 'pry'
-require_relative './memorable.rb'
+#require_relative './memorable.rb'
 
 class Song
-  extend Memorable
-  #include Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  #extend Findable
+  #include PARAMABLE
   
   attr_accessor :name
   attr_reader :artist
